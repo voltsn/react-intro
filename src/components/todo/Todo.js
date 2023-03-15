dffba91466e6f472b2fdedd00267c9d6df79f8cb
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-03-15 10:34:28 +0100

    Refactor

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -2,7 +2,8 @@ import './Todo.css';
 import { useState } from 'react';
 
 const Todo = () => {
-    const [todos, setTodo] = useState([{'id':0, 'task': 'Take out the trash'}, {'id':1, 'task': 'Feed the cat'}, {'id':2, 'task': 'Check emails'}]);
+    const initialTodos = [{'id':0, 'task': 'Take out the trash'}, {'id':1, 'task': 'Feed the cat'}, {'id':2, 'task': 'Check emails'}]
+    const [todos, setTodo] = useState(initialTodos);
 
     function handleClick(event){
         event.preventDefault();
@@ -21,15 +22,17 @@ const Todo = () => {
     }
 
     return <div className='container'>
-            <form class='form'>
+            <form className='form'>
                 <input className='todo-input' id="todo"type="text" name="todo" placeholder='Type a new todo'/>
                 <button className='btn btn--blue' onClick={handleClick}>Add Todo</button>
             </form>
             <div>
                 <h2 className="sub_heading">Todos</h2>
-                <ul className='todolist'>{todos.map(todo => (
-                    <li className='todo' key={todo.id}> <input type='checkbox'/>{todo.task}</li>
-                ))}</ul>
+                <ul className='todolist'>
+                    {todos.map(todo => (
+                        <li className='todo' key={todo.id}> <input type='checkbox'/>{todo.task}</li>
+                    ))}
+                </ul>
             </div>
         </div>;
 };
